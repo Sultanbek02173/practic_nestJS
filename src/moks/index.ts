32b9c33e6ac0a681766ffadbc7b68004e10dfc42
Sultanbek <sultan.02173@gmail.com>
2025-01-14 18:35:47 +0600

    init repozitory

---
+++ src/moks/index.ts
@@ -0,0 +1,116 @@
+export const users = [
+    {
+        "id": "678521f21c917eedeaffaa86",
+        "email": "bauer_petersen@delphide.nec",
+        "roles": [
+          "guest",
+          "admin"
+        ],
+        "apiKey": "234777dd-011f-4f9d-89d0-a635d07e08bd",
+        "profile": {
+          "dob": "1992-01-24",
+          "name": "Bauer Petersen",
+          "about": "Elit sint aute occaecat pariatur incididunt Lorem officia aliquip esse anim duis. Id commodo elit deserunt amet consequat occaecat.",
+          "address": "45 Jackson Street, Elliston, Palau",
+          "company": "Delphide",
+          "location": {
+            "lat": -54.191549,
+            "long": 7.598793
+          }
+        },
+        "username": "bauer92",
+        "createdAt": "2010-10-01T20:06:12.499Z",
+        "updatedAt": "2010-10-02T20:06:12.499Z"
+      },
+      {
+        "id": "678521f2ec02db4203e09506",
+        "email": "dorothy_santos@microluxe.kaufen",
+        "roles": [
+          "owner",
+          "admin"
+        ],
+        "apiKey": "44104cd7-52d7-4edb-8789-d11b04ba39fd",
+        "profile": {
+          "dob": "1994-03-08",
+          "name": "Dorothy Santos",
+          "about": "Elit labore est adipisicing et officia aute excepteur ea nostrud anim nostrud deserunt. Culpa eiusmod anim dolore incididunt ut sit id reprehenderit ex proident ut consequat adipisicing.",
+          "address": "9 Holmes Lane, Tyro, Utah",
+          "company": "Microluxe",
+          "location": {
+            "lat": -14.156973,
+            "long": 55.395185
+          }
+        },
+        "username": "dorothy94",
+        "createdAt": "2010-04-05T22:08:04.290Z",
+        "updatedAt": "2010-04-06T22:08:04.290Z"
+      },
+      {
+        "id": "678521f2831db49625dcdb0c",
+        "email": "mcclain_gilbert@nitracyr.sexy",
+        "roles": [
+          "guest",
+          "member"
+        ],
+        "apiKey": "059500d2-f63d-49b4-bf0e-653ede07f78d",
+        "profile": {
+          "dob": "1991-12-03",
+          "name": "Mcclain Gilbert",
+          "about": "Do pariatur commodo non mollit eiusmod eu ad dolor ad sint exercitation. Pariatur dolore minim Lorem aliqua dolore et.",
+          "address": "39 Seba Avenue, Dunlo, Maine",
+          "company": "Nitracyr",
+          "location": {
+            "lat": 64.245923,
+            "long": -12.244305
+          }
+        },
+        "username": "mcclain91",
+        "createdAt": "2012-04-22T15:29:20.449Z",
+        "updatedAt": "2012-04-23T15:29:20.449Z"
+      },
+      {
+        "id": "678521f260aabf75bea88089",
+        "email": "shari_mayo@xylar.nrw",
+        "roles": [
+          "owner",
+          "admin"
+        ],
+        "apiKey": "90501a9c-c8da-4ebe-a3f0-bf0b59a426e9",
+        "profile": {
+          "dob": "1991-02-23",
+          "name": "Shari Mayo",
+          "about": "Ea esse labore laboris minim Lorem nulla consequat dolor amet. Voluptate consectetur cillum irure dolor anim occaecat est sunt qui consectetur nisi laborum reprehenderit velit.",
+          "address": "82 Losee Terrace, Alamo, American Samoa",
+          "company": "Xylar",
+          "location": {
+            "lat": -78.155935,
+            "long": 148.281323
+          }
+        },
+        "username": "shari91",
+        "createdAt": "2013-02-13T14:43:23.101Z",
+        "updatedAt": "2013-02-14T14:43:23.101Z"
+      },
+      {
+        "id": "678521f2f1e06d3569a9f720",
+        "email": "luella_carter@sultrax.exposed",
+        "roles": [
+          "member"
+        ],
+        "apiKey": "a90c7480-3879-4cf1-9986-4a5f993a4550",
+        "profile": {
+          "dob": "1989-08-08",
+          "name": "Luella Carter",
+          "about": "Ea cupidatat do et in et non exercitation in sunt. Proident qui laborum eu mollit dolor aute nulla do proident do ullamco aute quis.",
+          "address": "57 Huntington Street, Bluffview, Oregon",
+          "company": "Sultrax",
+          "location": {
+            "lat": -78.099859,
+            "long": -128.131494
+          }
+        },
+        "username": "luella89",
+        "createdAt": "2012-06-17T11:47:10.266Z",
+        "updatedAt": "2012-06-18T11:47:10.266Z"
+      }
+]
