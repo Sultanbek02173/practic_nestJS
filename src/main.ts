034507cc15531526fa0da14c84ad8c34f9bb71ad
Sultanbek <sultan.02173@gmail.com>
2025-01-24 22:35:36 +0600

    port add

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     .build()
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(PORT);
+  await app.listen(PORT || 3000);
 }
 bootstrap();
